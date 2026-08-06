945452e834f7812d6c261d54751f18f78b9426e5
mdwasifulkabir <wasifulkabir9016@gmail.com>
2026-08-06 16:49:08 +0800

    Make first migration

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Quote(models.Model):
   quote = models.TextField()
-  source = models.Charfield()
+  source = models.CharField(max_length = 50)
 
   def __str__(self):
     return self.quote
--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics
+from .models import Quote
+from .serializers import QuoteSerializer
 
 # Create your views here.
+class QuoteListCreate(generics.ListCreateAPIView):
+  queryset = Quote.objects.all()
+  serializer_class = QuoteSerializer
